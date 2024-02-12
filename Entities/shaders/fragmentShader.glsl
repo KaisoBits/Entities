a6cee3d14e51e2decf518d09633b24c4b8b338dd
KaisoBits <31374466+KaisoBits@users.noreply.github.com>
2024-02-12 22:44:31 +0100

    Simplify shader & fix gitignore for .obj

--- Entities/shaders/fragmentShader.glsl
+++ Entities/shaders/fragmentShader.glsl
@@ -6,13 +6,8 @@ in vec2 textureCoord;
 
 uniform vec4 color = vec4(1.0, 0.0, 0.0, 1.0);
 uniform sampler2D texture0;
-uniform sampler2D texture1;
-uniform float time;
 
 void main()
 {
-	float zeroToOne = ((sin(time) + 1.0) / 2.0);
-	float multiplier = zeroToOne * 0.5 + 0.5;
-
-	FragColor = mix(texture(texture0, vec2(textureCoord.x, textureCoord.y)), texture(texture1, vec2(textureCoord.x, textureCoord.y)), zeroToOne);
+	FragColor = texture(texture0, vec2(textureCoord.x, textureCoord.y));
 }
